0d275a44cf91c806f29bbce54d8acd368105c8d9
Kadir Zukorlic <kadirzukorlic@gmail.com>
2023-05-10 01:48:17 +0200

    Add parsed body and write message from input

--- app.js
+++ app.js
@@ -15,7 +15,19 @@ const server = http.createServer((req, res) => {
 		return res.end()
 	}
 	if (url === '/message' && method === 'POST') {
-		fs.writeFileSync('message.txt', 'message from node')
+		const body = []
+
+		req.on('data', (chunk) => {
+			console.log(chunk)
+			body.push(chunk)
+		})
+
+		req.on('end', () => {
+			const parsedBody = Buffer.concat(body).toString()
+			const message = parsedBody.split('=')[1] // splits message=hamza and taking second item in array which is hamza
+			fs.writeFileSync('message.txt', message)
+		})
+
 		res.statusCode = 302
 		res.setHeader('Location', '')
 		return res.end()
