a26a0aca39cf0ea1e77cdb94debe5b39d8ab8b67
Kadir Zukorlic <kadirzukorlic@gmail.com>
2023-05-09 22:43:09 +0200

    Send html from server

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-	console.log(req)
+	console.log(req.url, req.method, req.headers)
+	// process.exist() // triggers hard exit on event loop
+	res.setHeader('Content-Type', 'text/html')
+	res.write('<html>')
+	res.write('<head><title>My first page</title></head>')
+	res.write('<body><h1>Hellow from node.js server</h1></body>')
+	res.write('</html>')
+	res.end()
 })
 
 server.listen(3000)
