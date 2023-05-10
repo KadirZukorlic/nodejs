56c85284253d504a535d9a50b0a3cee0d5ef17b4
Kadir Zukorlic <kadirzukorlic@gmail.com>
2023-05-11 01:49:41 +0200

    Assignment one

---
+++ Assignment/routes.js
@@ -0,0 +1,42 @@
+const fs = require('fs')
+
+const requestHandler = (req, res) => {
+	const url = req.url
+	const method = req.method
+    
+	if (url === '/') {
+		res.setHeader('Content-Type', 'text/html')
+		res.write('<html>')
+		res.write('<head><title>Hello world!</title></head>')
+		res.write('<h1>hello world</h1>')
+		res.write(
+			'<body><form action="/create-user" method="POST"><input type="text" name="user" /><button type="submit">Submit user</button></form></body>'
+		)
+		res.write('</html>')
+		return res.end()
+	}
+
+	if (url === '/create-user' && method === 'POST') {
+		const body = []
+
+		req.on('data', (chunk) => {
+			body.push(chunk)
+		})
+
+		return req.on('end', () => {
+			const parsedBody = Buffer.concat(body).toString()
+			const user = parsedBody.split('=')[1].replaceAll('+', ' ') // splits message=hamza and taking second item in array which is hamza
+			console.log(user)
+		})
+	}
+	if (url === '/users') {
+		res.setHeader('Content-Type', 'text/html')
+		res.write('<html>')
+		res.write('<head><title>Hello world!</title></head>')
+		res.write(`<ul><li>Kadir</li><li>Hamza</li><li>Amina</li></ul>`)
+		res.write('</html>')
+		return res.end()
+	}
+}
+
+module.exports = requestHandler
