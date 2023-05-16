3fa8dd6b0338cb4e734a068f30475a8c86ce2c97
Kadir Zukorlic <kadirzukorlic@gmail.com>
2023-05-16 16:51:44 +0200

    Dynamically render products with pug

--- routes/shop.js
+++ routes/shop.js
@@ -8,8 +8,8 @@ const adminData = require('./admin')
 const router = express.Router()
 
 router.get('/', (req, res, next) => {
-	console.log(adminData.products, 'shop.js')
-	res.sendFile(path.join(rootDir, 'views', 'shop.html'))
+	const products = adminData.products
+	res.render('shop', { prods: products, docTitle: 'Shop' })
 })
 
 module.exports = router
