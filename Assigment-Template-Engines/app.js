9f49a7af5705caaaa85498fa3e8f8ebbd940cb2c
Kadir Zukorlic <kadirzukorlic@gmail.com>
2023-05-20 03:57:42 +0200

    Assignment logic done css left

--- Assigment-Template-Engines/app.js
+++ Assigment-Template-Engines/app.js
@@ -3,13 +3,23 @@ const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
 
+const usersData = require('./routes/user')
+
 const app = express()
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(
+	express.static(path.join(__dirname, '/Assigment-Template-Engines/public'))
+)
 
 app.set('view engine', 'ejs')
 app.set('views', 'views')
 
+app.use(usersData.routes)
+
 app.use((req, res, next) => {
-	res.send('<h1>radi li server uopste</h1>')
+	console.log(usersData.users)
+	res.render('form-assignment', { pageTitle: 'Add User' })
 })
 
 app.listen(3000)
---
+++ Assigment-Template-Engines/routes/user.js
@@ -0,0 +1,19 @@
+const express = require('express')
+
+const router = express.Router()
+
+const users = []
+
+router.get('/users', (req, res, next) => {
+	res.render('users-assignment', {
+		users: users
+	})
+})
+
+router.post('/', (req, res, next) => {
+	users.push({ name: req.body.name })
+	res.redirect('/')
+})
+
+exports.routes = router
+exports.users = users
