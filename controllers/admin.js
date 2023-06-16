e786e0367762e905085313f1b2b57b7a99768953
Kadir Zukorlic <kadirzukorlic@gmail.com>
2023-06-16 03:15:59 +0200

    Edit product logic

--- controllers/admin.js
+++ controllers/admin.js
@@ -14,7 +14,7 @@ exports.postAddProduct = (req, res, next) => {
 	const description = req.body.description
 	const price = req.body.price
 
-	const product = new Product(title, imageUrl, description, price)
+	const product = new Product(null, title, imageUrl, description, price)
 	product.save()
 	res.redirect('/')
 }
@@ -37,6 +37,25 @@ exports.getEditProduct = (req, res, next) => {
 			product: product
 		})
 	})
+}
+
+exports.postEditProduct = (req, res, next) => {
+	const prodId = req.body.productId // if (editing) in edit-product.ejs we rendered hidden input with name productId, and value="<%= product.id %>" thats why req.body.productId gives us an actual id
+	const updatedTitle = req.body.title
+	const updatedImageUrl = req.body.imageUrl
+	const updatedDescription = req.body.description
+	const updatedPrice = req.body.price
+	const updatedProduct = new Product(
+		prodId,
+		updatedTitle,
+		updatedImageUrl,
+		updatedDescription,
+		updatedPrice
+	)
+
+	updatedProduct.save()
+
+	res.redirect('/admin/products')
 }
 
 exports.getProducts = (req, res, next) => {
