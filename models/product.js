4354b65082fc15f5e24e7b9809c6f4a62cb2c0e8
Kadir Zukorlic <kadirzukorlic@gmail.com>
2023-07-01 02:33:07 +0200

    Add delete product

--- models/product.js
+++ models/product.js
@@ -3,6 +3,8 @@ const path = require('path')
 
 const rootDir = require('../util/path')
 
+const Cart = require('./cart')
+
 const p = path.join(rootDir, 'data', 'products.json')
 
 const getProductsFromFile = (callback) => {
@@ -41,6 +43,18 @@ module.exports = class Product {
 					console.log(err, 'ERROR')
 				})
 			}
+		})
+	}
+
+	static deleteById(id) {
+		getProductsFromFile((products) => {
+			const product = products.find((prod) => prod.id === id)
+			const updatedProducts = products.filter((prod) => prod.id !== id)
+			fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
+				if (!err) {
+					Cart.deleteProduct(id, product.price)
+				}
+			})
 		})
 	}
 
