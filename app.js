a3ac087e1560ee589dfc2cbca0d22455898a0de5
Kadir Zukorlic <kadirzukorlic@gmail.com>
2023-07-03 02:38:50 +0200

    Remove hbs imports since we wont use it anymore

--- app.js
+++ app.js
@@ -2,18 +2,9 @@ const path = require('path')
 
 const express = require('express')
 const bodyParser = require('body-parser')
-// const expressHbs = require('express-handlebars')
 
 const app = express()
-
-// app.engine(
-// 	'hbs',
-// 	expressHbs({
-// 		layoutsDir: 'views/layouts',
-// 		defaultLayout: 'main',
-// 		extname: 'hbs'
-// 	})
-// )
+const db = require('./util/database')
 
 app.set('view engine', 'ejs')
 app.set('views', 'views')
