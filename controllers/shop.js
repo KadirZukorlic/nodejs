7c7b088b6a8f7f232e860bbe2539d365d790d9cd
Kadir Zukorlic <kadirzukorlic@gmail.com>
2023-07-20 02:43:37 +0200

    Fetch products from db with sequalize

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,12 +2,12 @@ const Product = require('../models/product')
 const Cart = require('../models/cart')
 
 exports.getProducts = (req, res, next) => {
-	Product.fetchAll()
-		.then(([rows]) => {
-			res.render('shop/product-list', {
-				prods: rows,
-				pageTitle: 'All Products',
-				path: '/products'
+	Product.findAll()
+		.then((products) => {
+			res.render('shop/index', {
+				prods: products,
+				pageTitle: 'Shop',
+				path: '/'
 			})
 		})
 		.catch((err) => console.log(err))
@@ -27,17 +27,15 @@ exports.getProduct = (req, res, next) => {
 }
 
 exports.getIndex = (req, res, next) => {
-	Product.fetchAll()
-		.then(([rows]) => {
+	Product.findAll()
+		.then((products) => {
 			res.render('shop/index', {
-				prods: rows,
+				prods: products,
 				pageTitle: 'Shop',
 				path: '/'
 			})
 		})
-		.catch((err) => {
-			console.log(err)
-		})
+		.catch((err) => console.log(err))
 }
 
 exports.postCart = (req, res, next) => {
