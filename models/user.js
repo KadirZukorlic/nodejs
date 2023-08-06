7bcf97718805469c04215dc5032255778ff9d358
Kadir Zukorlic <kadirzukorlic@gmail.com>
2023-08-06 17:31:03 +0200

    Define user model

--- models/user.js
+++ models/user.js
@@ -1,1 +1,18 @@
 // id, name, last name, email
+
+const Sequelize = require('sequelize')
+
+const sequalize = require('../util/database')
+
+const User = sequalize.define('user', {
+	id: {
+		type: Sequelize.INTEGER,
+		autoIncrement: true,
+		allowNull: false,
+		primaryKey: true
+	},
+	name: Sequelize.STRING,
+	email: Sequelize.STRING
+})
+
+module.exports = User
