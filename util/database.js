b18245cbe06a079ab3508a7f836effd5fee2072b
Kadir Zukorlic <kadirzukorlic@gmail.com>
2023-08-06 17:18:32 +0200

    Install dotenv and remove database from gitignore

---
+++ util/database.js
@@ -0,0 +1,14 @@
+const Sequelize = require('sequelize')
+require('dotenv').config()
+
+const sequelize = new Sequelize(
+	'nodejs-shop',
+	'root',
+	process.env.DB_PASSWORD,
+	{
+		dialect: 'mysql',
+		host: 'localhost'
+	}
+)
+
+module.exports = sequelize
