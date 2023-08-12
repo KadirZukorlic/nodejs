352f92a9e0771db67ae05bb573deed4ac376ac58
Kadir Zukorlic <kadirzukorlic@gmail.com>
2023-08-13 01:31:39 +0200

    Set cart relations

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app = express()
 const sequelize = require('./util/database')
 const Product = require('./models/product')
 const User = require('./models/user')
+const Cart = require('./models/cart')
 
 app.set('view engine', 'ejs')
 app.set('views', 'views')
@@ -14,6 +15,7 @@ app.set('views', 'views')
 const adminRoutes = require('./routes/admin')
 const errorController = require('./controllers/error')
 const shopRoutes = require('./routes/shop')
+const CartItem = require('./models/cart-item')
 
 // parses incoming requests available in req.body
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -36,6 +38,11 @@ app.use(errorController.get404)
 // Sequalize create relations of `Product` and `User`. check => https://sequelize.org/docs/v6/core-concepts/assocs/ for more info
 Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' })
 User.hasMany(Product)
+
+User.hasOne(Cart)
+Cart.belongsTo(User)
+Cart.belongsToMany(Product, { through: CartItem })
+Product.belongsToMany(Cart, { through: CartItem })
 
 // Sequalize create tables
 sequelize
