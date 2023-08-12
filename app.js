11113d6d7532b470472eab06356690708c06523e
Kadir Zukorlic <kadirzukorlic@gmail.com>
2023-08-13 00:50:13 +0200

    Create dummy user

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser')
 
 const app = express()
 const sequelize = require('./util/database')
+const Product = require('./models/product')
+const User = require('./models/user')
 
 app.set('view engine', 'ejs')
 app.set('views', 'views')
@@ -17,20 +19,46 @@ const shopRoutes = require('./routes/shop')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
 
+app.use((req, res, next) => {
+	User.findByPk(1)
+		.then((user) => {
+			req.user = user
+			next()
+		})
+		.catch((err) => console.log(err))
+})
+
 app.use('/admin', adminRoutes)
 app.use(shopRoutes)
 
 app.use(errorController.get404)
 
+// Sequalize create relations of `Product` and `User`. check => https://sequelize.org/docs/v6/core-concepts/assocs/ for more info
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' })
+User.hasMany(Product)
+
+// Sequalize create tables
 sequelize
+	// .sync({ force: true })
 	.sync()
 	.then((result) => {
+		return User.findByPk(1)
 		// console.log(result)
-        app.listen(3000)
+	})
+	.then((user) => {
+		if (!user) {
+			return User.create({
+				name: 'Kadir',
+				email: 'test@test.com'
+			})
+		}
+		return user
+	})
+	.then((user) => {
+		// console.log(user)
+		app.listen(3000)
 	})
 	.catch((err) => console.log(err))
-
-
 
 // This is what is under the hood in express
 // app.listen = function listen() {
