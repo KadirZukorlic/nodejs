9494d86100fd0247ef1ebf2cbcfadf24af1c70d0
Kadir Zukorlic <kadirzukorlic@gmail.com>
2023-09-02 01:11:02 +0200

    Connect to mongodb

--- app.js
+++ app.js
@@ -4,77 +4,36 @@ const express = require('express')
 const bodyParser = require('body-parser')
 
 const app = express()
-const sequelize = require('./util/database')
-const Product = require('./models/product')
-const User = require('./models/user')
-const Cart = require('./models/cart')
 
 app.set('view engine', 'ejs')
 app.set('views', 'views')
 
-const adminRoutes = require('./routes/admin')
 const errorController = require('./controllers/error')
-const shopRoutes = require('./routes/shop')
-const CartItem = require('./models/cart-item')
-const Order = require('./models/order')
-const OrderItem = require('./models/order-item')
+
+// const adminRoutes = require('./routes/admin')
+// const shopRoutes = require('./routes/shop')
+
+const mongoConnect = require('./util/database')
 
 // parses incoming requests available in req.body
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use((req, res, next) => {
-	User.findByPk(1)
-		.then((user) => {
-			req.user = user
-			next()
-		})
-		.catch((err) => console.log(err))
+	// User.findByPk(1)
+	// 	.then((user) => {
+	// 		req.user = user
+	// 		next()
+	// 	})
+	// 	.catch((err) => console.log(err))
 })
 
-app.use('/admin', adminRoutes)
-app.use(shopRoutes)
+// app.use('/admin', adminRoutes)
+// app.use(shopRoutes)
 
 app.use(errorController.get404)
 
-// Sequalize create relations of `Product` and `User`. check => https://sequelize.org/docs/v6/core-concepts/assocs/ for more info
-Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' })
-User.hasMany(Product)
-
-User.hasOne(Cart)
-Cart.belongsTo(User)
-Cart.belongsToMany(Product, { through: CartItem })
-Product.belongsToMany(Cart, { through: CartItem })
-Order.belongsTo(User)
-User.hasMany(Order)
-Order.belongsToMany(Product, { through: OrderItem })
-
-// Sequalize create tables
-sequelize
-	// .sync({ force: true })
-	.sync()
-	.then((result) => {
-		return User.findByPk(1)
-		// console.log(result)
-	})
-	.then((user) => {
-		if (!user) {
-			return User.create({
-				name: 'Kadir',
-				email: 'test@test.com'
-			})
-		}
-		return user
-	})
-	.then((user) => {
-		// console.log(user)
-		user.createCart()
-		app.listen(3000)
-	})
-	.catch((err) => console.log(err))
-
-// This is what is under the hood in express
-// app.listen = function listen() {
-//     var server = http.createServer(this);
-//     return server.listen.apply(server, arguments);
-//   };
+mongoConnect((client) => {
+	console.log(client)
+	app.listen(3000)
+})
--- util/database.js
+++ util/database.js
@@ -1,14 +1,15 @@
-const Sequelize = require('sequelize')
-require('dotenv').config()
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
 
-const sequelize = new Sequelize(
-	'nodejs-shop',
-	'root',
-	process.env.DB_PASSWORD,
-	{
-		dialect: 'mysql',
-		host: 'localhost'
-	}
-)
+const mongoConnect = (callback) => {
+	MongoClient.connect(
+		'mongodb+srv://Kadir:starfallkacobre1@cluster0.h22nx.mongodb.net/?retryWrites=true&w=majority'
+	)
+		.then((client) => {
+			console.log('Connected!')
+			callback(client)
+		})
+		.catch((error) => console.log(error))
+}
 
-module.exports = sequelize
+module.exports = mongoConnect
