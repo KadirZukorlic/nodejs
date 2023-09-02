e32763072464cb5b8eb7e4d01fece46c1e223ab5
Kadir Zukorlic <kadirzukorlic@gmail.com>
2023-09-02 03:55:48 +0200

    Fetch all products with mongo db

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.set('views', 'views')
 const errorController = require('./controllers/error')
 
 const adminRoutes = require('./routes/admin')
-// const shopRoutes = require('./routes/shop')
+const shopRoutes = require('./routes/shop')
 
 const mongoConnect = require('./util/database').mongoConnect
 
@@ -30,7 +30,7 @@ app.use((req, res, next) => {
 })
 
 app.use('/admin', adminRoutes)
-// app.use(shopRoutes)
+app.use(shopRoutes)
 
 app.use(errorController.get404)
 
