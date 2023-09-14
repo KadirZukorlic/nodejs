13fe9a44d3d160a175933a7345bb759c2ab905da
Kadir Zukorlic <kadirzukorlic@gmail.com>
2023-09-15 00:30:31 +0200

    Add orders

--- controllers/shop.js
+++ controllers/shop.js
@@ -95,28 +95,8 @@ exports.getCheckout = (req, res, next) => {
 exports.postOrder = (req, res, next) => {
 	let fetchedCart = []
 	req.user
-		.getCart()
-		.then((cart) => {
-			fetchedCart = cart
-			return cart.getProducts()
-		})
-		.then((products) => {
-			return req.user
-				.createOrder()
-				.then((order) => {
-					order.addProducts(
-						products.map((product) => {
-							product.orderItem = { quantity: product.cartItem.quantity }
-							return product
-						})
-					)
-				})
-				.catch((error) => console.log(error))
-		})
-		.then((result) => {
-			return fetchedCart.setProducts(null)
-		})
-		.then((result) => {
+		.addOrder()
+		.then(() => {
 			res.redirect('/orders')
 		})
 		.catch((error) => {
