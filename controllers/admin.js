eea6fc0fa0e3faec1cea8ee24fbebb408599ef94
Kadir Zukorlic <kadirzukorlic@gmail.com>
2023-10-02 01:00:55 +0200

    Updating product

--- controllers/admin.js
+++ controllers/admin.js
@@ -56,16 +56,15 @@ exports.postEditProduct = (req, res, next) => {
 	const updatedDescription = req.body.description
 	const updatedPrice = req.body.price
 
-	const product = new Product(
-		updatedTitle,
-		updatedPrice,
-		updatedDescription,
-		updatedImageUrl,
-		prodId
-	)
+	Product.findById(prodId)
+		.then((product) => {
+			product.title = updatedTitle
+			product.price = updatedPrice
+			product.description = updatedDescription
+			product.imageUrl = updatedImageUrl
 
-	product
-		.save()
+			return product.save()
+		})
 		.then((result) => {
 			console.log('UPDATED PRODUCT')
 			res.redirect('/admin/products')
@@ -74,7 +73,7 @@ exports.postEditProduct = (req, res, next) => {
 }
 
 exports.getProducts = (req, res, next) => {
-	Product.fetchAll()
+	Product.find()
 		.then((products) => {
 			res.render('admin/products', {
 				prods: products,
