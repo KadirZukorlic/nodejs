f0485bef641f220716e4f9a59b1d51f28a45b2e4
Kadir Zukorlic <kadirzukorlic@gmail.com>
2023-10-02 01:27:46 +0200

    Create dummy user with mongoose

--- app.js
+++ app.js
@@ -16,20 +16,20 @@ const errorController = require('./controllers/error')
 const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
-// const User = require('./models/user')
+const User = require('./models/user')
 
 // parses incoming requests available in req.body
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
 
-// app.use((req, res, next) => {
-// 	User.findById('64fee4f4c6359b60f9539931')
-// 		.then((user) => {
-// 			req.user = new User(user.name, user.email, user.cart, user._id)
-// 			next()
-// 		})
-// 		.catch((err) => console.log(err))
-// })
+app.use((req, res, next) => {
+	User.findById('651a002106390654b5397449')
+		.then((user) => {
+			req.user = user
+			next()
+		})
+		.catch((err) => console.log(err))
+})
 
 app.use('/admin', adminRoutes)
 app.use(shopRoutes)
@@ -39,6 +39,18 @@ app.use(errorController.get404)
 mongoose
 	.connect(process.env.MONGODB_URL)
 	.then(() => {
+		User.findOne().then((user) => {
+			if (!user) {
+				const user = new User({
+					name: 'Hamza',
+					email: 'hamza@test.com',
+					cart: {
+						items: []
+					}
+				})
+				user.save()
+			}
+		})
 		app.listen(3000)
 	})
 	.catch((err) => console.log(err))
