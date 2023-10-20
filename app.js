5dac9051d2abb8927d724b5d9589875c81b9719e
Kadir Zukorlic <kadirzukorlic@gmail.com>
2023-10-20 18:50:38 +0200

    Signup user and hash password

--- app.js
+++ app.js
@@ -59,18 +59,6 @@ app.use(errorController.get404)
 mongoose
 	.connect(process.env.MONGODB_URL)
 	.then(() => {
-		User.findOne().then((user) => {
-			if (!user) {
-				const user = new User({
-					name: 'Hamza',
-					email: 'hamza@test.com',
-					cart: {
-						items: []
-					}
-				})
-				user.save()
-			}
-		})
 		app.listen(3000)
 	})
 	.catch((err) => console.log(err))
