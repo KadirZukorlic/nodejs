00956e6dad475e55c1b182552979d14089b93436
Kadir Zukorlic <kadirzukorlic@gmail.com>
2023-11-09 01:34:10 +0100

    Add basic input validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const User = require('../models/user')
 
 const crypto = require('crypto')
 const bcrypt = require('bcryptjs')
+const { validationResult } = require('express-validator')
 
 exports.getLogin = (req, res, next) => {
 	res.render('auth/login', {
@@ -54,6 +55,16 @@ exports.postSignup = (req, res, next) => {
 	const email = req.body.email
 	const password = req.body.password
 	const confirmPassword = req.body.confirmPassword
+	const errors = validationResult(req)
+
+	if (!errors.isEmpty()) {
+		console.log('errorsssss: ', errors.array())
+		return res.status(422).render('auth/signup', {
+			path: '/signup',
+			pageTitle: 'Signup',
+			errorMessage: errors.array()[0].msg
+		})
+	}
 
 	User.findOne({ email: email })
 		.then((userDoc) => {
