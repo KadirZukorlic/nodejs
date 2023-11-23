94078858b631443bcf25662082503a10c8d0e313
Kadir Zukorlic <kadirzukorlic@gmail.com>
2023-11-23 02:11:48 +0100

    Add stripe needs fix an error with line_items

--- app.js
+++ app.js
@@ -102,6 +102,7 @@ app.get('/500', errorController.get500)
 app.use(errorController.get404)
 
 app.use((error, req, res, next) => {
+	console.log(error)
 	// res.redirect('/500')
 	res.status(500).render('500', {
 		pageTitle: 'Error!',
--- controllers/shop.js
+++ controllers/shop.js
@@ -1,11 +1,13 @@
+require('dotenv').config()
+
 const fs = require('fs')
 const path = require('path')
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 
 const PDFDocument = require('pdfkit')
 
 const Product = require('../models/product')
 const Order = require('../models/order')
-const { errorMonitor } = require('events')
 
 const ITEMS_PER_PAGE = 1
 
@@ -136,21 +138,73 @@ exports.postCartDeleteProduct = (req, res, next) => {
 }
 
 exports.getCheckout = (req, res, next) => {
+	let products
+	let total = 0
 	req.user
 		.populate('cart.items.productId')
 		// .execPopulate()
 		.then((user) => {
-			const products = user.cart.items
-			let total = 0
+			products = user.cart.items
+			total = 0
 			products.forEach((p) => {
 				total += p.quantity * p.productId.price
 			})
+
+			return stripe.checkout.sessions.create({
+				payment_method_types: ['card'],
+				// Please see https://stripe.com/docs/payments/checkout/migrating-prices for more information.
+				line_items: products.map((p) => {
+					return {
+						name: p.productId.title,
+						description: p.productId.description,
+						amount: p.productId.price * 100,
+						currency: 'usd',
+						quantity: p.quantity
+					}
+				}),
+				success_url:
+					req.protocol + '://' + req.get('host') + '/checkout/success',
+				cancel_url: req.protocol + '://' + req.get('host') + '/checkout/cancel'
+			})
+		})
+		.then((session) => {
 			res.render('shop/checkout', {
 				path: '/checkout',
 				pageTitle: 'Checkout',
 				products: products,
-				totalSum: total
+				totalSum: total,
+				sessionId: session.id
 			})
+		})
+		.catch((err) => {
+			const error = new Error(err)
+			error.httpStatusCode = 500
+			return next(error)
+		})
+}
+
+exports.getCheckoutSuccess = (req, res, next) => {
+	req.user
+		.populate('cart.items.productId')
+		// .execPopulate()
+		.then((user) => {
+			const products = user.cart.items.map((i) => {
+				return { quantity: i.quantity, product: { ...i.productId._doc } }
+			})
+			const order = new Order({
+				user: {
+					email: req.user.email,
+					userId: req.user
+				},
+				products: products
+			})
+			return order.save()
+		})
+		.then((result) => {
+			return req.user.clearCart()
+		})
+		.then(() => {
+			res.redirect('/orders')
 		})
 		.catch((err) => {
 			const error = new Error(err)
